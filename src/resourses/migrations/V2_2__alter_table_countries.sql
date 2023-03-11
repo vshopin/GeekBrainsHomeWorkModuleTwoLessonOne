24f2dc6241e30ae7d505abdab49b356223fba81b
shopi <shopin_va@mail.ru>
2023-03-11 21:05:05 +0700

    completed tasks of the lesson one

---
+++ src/resourses/migrations/V2_2__alter_table_countries.sql
@@ -0,0 +1,19 @@
+alter table _countries
+    drop title_ua,
+    drop title_be,
+    drop title_en,
+    drop title_es,
+    drop title_pt,
+    drop title_de,
+    drop title_fr,
+    drop title_it,
+    drop title_pl,
+    drop title_ja,
+    drop title_lt,
+    drop title_lv,
+    drop title_cz;
+
+alter table _countries
+    change column country_id id int not null auto_increment primary key,
+    change column title_ru title varchar(150) not null;
+alter table _countries add index(title);
